10907fbfac00b6ff36a2c9ea18e79c80f344c860
Bryce Dorn <brycedorn@gmail.com>
2018-04-01 01:24:58 -0500

    fix wildcard exports

--- dist/index.js
+++ dist/index.js
@@ -11,19 +11,21 @@ var _lego2 = _interopRequireDefault(_lego);
 
 var _colors = require('./colors');
 
-var _colors2 = _interopRequireDefault(_colors);
+var colors = _interopRequireWildcard(_colors);
 
 var _shapes = require('./shapes');
 
-var _shapes2 = _interopRequireDefault(_shapes);
+var shapes = _interopRequireWildcard(_shapes);
 
 var _sizes = require('./sizes');
 
 var _sizes2 = _interopRequireDefault(_sizes);
 
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = _lego2.default;
-exports.colors = _colors2.default;
-exports.shapes = _shapes2.default;
+exports.colors = colors;
+exports.shapes = shapes;
 exports.sizes = _sizes2.default;
